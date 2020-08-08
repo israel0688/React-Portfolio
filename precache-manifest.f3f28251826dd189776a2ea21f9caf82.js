b1bc82f1983ccbc6dfe9bf11c3a662ce12a20015
Israel Berlanga <israel0688@hotmail.com>
2020-08-07 22:57:05 -0500

    Updates

---
+++ precache-manifest.f3f28251826dd189776a2ea21f9caf82.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e204d6387e3f2a764091f7f0776e004",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "3072007cfde4c475bb82",
+    "url": "/React-Portfolio/static/css/main.6084d91d.chunk.css"
+  },
+  {
+    "revision": "851c2e4ec293ef011028",
+    "url": "/React-Portfolio/static/js/2.67627835.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.67627835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3072007cfde4c475bb82",
+    "url": "/React-Portfolio/static/js/main.70572027.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/React-Portfolio/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/React-Portfolio/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "bf78f5ef1eb8a37f64d9795b35bd8462",
+    "url": "/React-Portfolio/static/media/cover-image.bf78f5ef.jpg"
+  },
+  {
+    "revision": "06f80b4cd0cf25bf445904500f8c0ff2",
+    "url": "/React-Portfolio/static/media/horiseon.06f80b4c.png"
+  },
+  {
+    "revision": "412fe63c44f80c6af4dd21b8d190ab99",
+    "url": "/React-Portfolio/static/media/runbuddy-image.412fe63c.png"
+  },
+  {
+    "revision": "ef50cb4ff23eafbf1c03440f49c81014",
+    "url": "/React-Portfolio/static/media/screenshot2.ef50cb4f.png"
+  },
+  {
+    "revision": "e38ba360929095f2069ec5b148395f3f",
+    "url": "/React-Portfolio/static/media/workoutworkers.e38ba360.png"
+  }
+]);
